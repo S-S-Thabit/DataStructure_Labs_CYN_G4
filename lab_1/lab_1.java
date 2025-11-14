87cd0eae88aea69f547141a203554a5a34fa0641
Salah Aldeen Samer Thabit <Eng.salahaldeen.thabit@gmail.com>
2025-11-15 00:25:17 +0300

    Update lab_1.java

--- lab_1/lab_1.java
+++ lab_1/lab_1.java
@@ -2,9 +2,9 @@ package lab_1;
 
 public class lab_1 {
     public static void main(String[] args) {
-        // 3 Scanner scanner=new Scanner(System.in);
+        
         Arrayss arr = new Arrayss();
-        // int []numbers={1,2,3};
+        
         int[] numbers = new int[3];
         arr.input(numbers);
         arr.traversal(numbers);
