90fd5181b5b2ba136b6f86efc15440d5764bc8de
S-S-Thabit <eng.salahaldeen.thabit@gmail.com>
2025-11-14 21:13:48 +0300

    lab_1

---
+++ lab_1/lab_1.java
@@ -0,0 +1,24 @@
+package lab_1;
+
+public class lab_1 {
+    public static void main(String[] args) {
+        // 3 Scanner scanner=new Scanner(System.in);
+        Array arr = new Array();
+        // int []numbers={1,2,3};
+        int[] numbers = new int[3];
+        arr.input(numbers);
+        arr.traversal(numbers);
+
+        arr.revers_traversal(numbers);
+
+        // System.out.println(Arrays.toString(numbers));
+
+        // for (int i=0; i<numbers.length;i++)
+        // System.out.println(numbers[i]);
+
+        // for(int num :numbers)
+        // System.out.println(num);
+        //
+
+    }
+}
