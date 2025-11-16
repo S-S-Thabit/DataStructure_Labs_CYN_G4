6a524c5b4ae8d47133a7b6d264709aa07d86bb69
S-S-Thabit <eng.salahaldeen.thabit@gmail.com>
2025-11-16 08:22:11 +0300

    labs have been created

---
+++ Array/Array.java
@@ -0,0 +1,71 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Array {
+    Scanner scanner = new Scanner(System.in);
+
+    public void traversal(int[] numbers) {
+        System.out.println("the traversal array elements are:");
+        System.out.println("numbers[index] = element.");
+        for (int i = 0; i < numbers.length; i++)
+            System.out.println("numbers[" + i + "]" + "  =  " + numbers[i]);
+
+    }
+
+    public void revers_traversal(int[] numbers) {
+        System.out.println("the reverse traversal array elements are:");
+
+        System.out.println("numbers[index] = element.");
+        for (int i = numbers.length - 1; i >= 0; i--)
+            System.out.println("numbers[" + i + "]" + "  =  " + numbers[i]);
+
+    }
+
+    public void input(int[] numbers) {
+        System.out.println("Please enter " + numbers.length + " numbers:");
+        for (int i = 0; i < numbers.length; i++)
+            numbers[i] = scanner.nextInt();
+
+    }
+
+    public void update(int a[], int index, int element){
+        if(index <0 || index>a.length)
+            System.out.println("index"+index+"not found");
+        else a[index]=element;
+    }
+
+    public void delete(int a[], int index){
+        if(index <0 || index>a.length)
+            System.out.println("index"+index+"not found");
+        else a[index]=0;
+    }
+
+    public int linearSearch(int []a, int val){
+        for(int i=0; i<a.length-1;i++){
+            if(a[i]==val)
+                return i;
+
+        }
+
+        return -1;
+    }
+
+
+    public int binarySearch(int []a, int findex,int lindex, int val){
+
+        while (findex<=lindex){
+            int mid = (findex+lindex)/2;
+            if (val==a[mid])
+                return mid;
+            if (val<a[mid])
+                lindex=mid-1;
+            if (val>a[mid])
+                findex=mid+1;
+
+        }
+        return -1;
+    }
+
+
+}
